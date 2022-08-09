7f63f775b2b0b2a4ebbd17e1f9827f6e47268c23
EduXimenes <e.ximenes17@hotmail.com>
2022-08-09 08:31:32 -0300

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Eduardo Ximenes on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
